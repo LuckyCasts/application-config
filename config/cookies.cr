19d8a2817220fb472577ce905a1e30dceb2643f9
Stephen Dolan <dolan.stephen1@gmail.com>
2021-03-06 15:46:42 -0500

    Initial commit

---
+++ config/cookies.cr
@@ -0,0 +1,17 @@
+require "./server"
+
+Lucky::Session.configure(&.key=("_lucky_jumpstart_session"))
+
+Lucky::CookieJar.configure do |settings|
+  settings.on_set = ->(cookie : HTTP::Cookie) {
+    # If ForceSSLHandler is enabled, only send cookies over HTTPS
+    cookie.secure(Lucky::ForceSSLHandler.settings.enabled)
+
+    # By default, don't allow reading cookies with JavaScript
+    cookie.http_only(true)
+
+    # You can set other defaults for cookies here. For example:
+    #
+    #    cookie.expires(1.year.from_now).domain("mydomain.com")
+  }
+end
